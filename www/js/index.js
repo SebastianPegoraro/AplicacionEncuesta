4bcf0b5cd6b2e534d264c2ba25af099e4f442075
SebastianPegoraro <pegorarosebastian@gmail.com>
2019-03-06 13:50:56 -0300

    commiteanding

--- www/js/index.js
+++ www/js/index.js
@@ -1,46 +1,45 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+mensaje("Iniciando");
+document.addEventListener("deviceready", onDeviceReady, false);
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+//iniciar base de datos, crear si no existe. consultar por una tabla, si no
+//devuelve filas, tengo que conectarme al servidor y leer los datos.
+function onDeviceReady() {
+mensaje("Comprobando datos");
+  var db = window.sqlitePlugin.openDatabase({ name: 'encuesta.db', location: 'default' }, function (db) {
+//crear las tablas si no existen
+    db.executeSql('CREATE TABLE  elecciones (id INTEGER,descripcion)');
+    db.executeSql('CREATE TABLE  encuestas (id INTEGER,titulo,fecha_inicio,fecha_cierre,fecha_creacion)');
+    db.executeSql('CREATE TABLE  opciones (id INTEGER,eleccion_id,tipo_id,pregunta_id,estado)');
+    db.executeSql('CREATE TABLE  preguntas (id INTEGER,descripcion,encuesta_id)');
+    db.executeSql('CREATE TABLE  tipos (id INTEGER,clase)');
+    db.executeSql('CREATE TABLE  usuarios (idUsuario INTEGER,nombre,password,tipo)');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+    mensaje("Leyendo datos");
 
-        console.log('Received Event: ' + id);
-    }
-};
+    db.transaction(function(tx) {
+    tx.executeSql('SELECT count(*) AS mycount FROM usuarios', [], function(tx, rs) {
+      //mensaje("filas: "+rs.rows.item(0).mycount);
+      if(rs.rows.item(0).mycount=='0')
+      {
+      window.location.href="init.html";
+      }
+      else {
+        window.location.href="login.html";
+      }
+    }, function(tx, error) {
+      mensaje('SELECT error: ' + error.message);
+    });
+  });
 
-app.initialize();
+
+}, function (error) {
+  mensaje("Error abriendo BD: "+ JSON.stringify(error));
+});
+}
+
+
+function mensaje(msg)
+{
+  $('#estado').append('<p>'+msg+'</p>');
+}
